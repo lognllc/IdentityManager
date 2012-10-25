f095518c832d3343a770b67463583fc945d013cd
Rex Sheng <shengning@gmail.com>
2012-10-25 13:14:34 +0800

    add demo

---
+++ Examples/Example/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Example
+//
+//  Created by Rex Sheng on 10/25/12.
+//  Copyright (c) 2012 LogN LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
