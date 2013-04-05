ebce5a896a8eda0b65b726f4853469cb69bf9b75
Rex Sheng <shengning@gmail.com>
2013-04-05 15:54:41 +0800

    email validation case insensitive

--- LNTextField.m
+++ LNTextField.m
@@ -59,7 +59,7 @@
 		@"9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21"
 		@"-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
         predicate = [NSPredicate predicateWithFormat:@"SELF matches %@", reg];
-        isValid |= [predicate evaluateWithObject:text];
+        isValid |= [predicate evaluateWithObject:text.lowercaseString];
 	}
     if (_validateType & LNTextValidateFlyFrontierEmployeeAccount) {
         reg = EMPLOYEE_PATTERN;
